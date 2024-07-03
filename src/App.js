2ccad92866759b857afcad61f1436a917bf8e9b4
Anirudh <97828169+AnirudhSharmagithub01@users.noreply.github.com>
2024-07-03 15:44:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
       <div className=''>
         <Header />
       </div>
-      <Spinner/>
+      // <Spinner/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
